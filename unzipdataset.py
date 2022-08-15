a510b38849d1f780048f35d96c6ebd1fffea7d31
karan-s-n <111242026+karan-s-n@users.noreply.github.com>
2022-08-15 21:48:23 +0530

    Update unzipdataset.py

--- unzipdataset.py
+++ unzipdataset.py
@@ -1,1 +1,12 @@
 
+import os, sys, tarfile
+def extract_iamdataset_file(tar_url, extract_path='./datasets/words/'):
+    print(tar_url)
+    tar = tarfile.open(tar_url, 'r')
+    for item in tar:
+        print(item)
+        tar.extract(item, extract_path)
+        if item.name.find(".tgz") != -1 or item.name.find(".tar") != -1:
+            extract(item.name, "./" + item.name[:item.name.rfind('/')])
+
+extract(r'D:\Work\A-Z-0-9-Handwritten-Character-Recognition-Crop-Tool\notebooks\IAM_Words\words.tgz',extract_path)
