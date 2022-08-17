32959727ee1d36592ae37a37bc405b85bcd95fea
karan-s-n <111242026+karan-s-n@users.noreply.github.com>
2022-08-17 20:21:08 +0530

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,26 +1,14 @@
-"""  
-Copyright (c) 2019-present NAVER Corp.
-MIT License
-"""
-
-# -*- coding: utf-8 -*-
 import numpy as np
 import cv2
 import math
-# -*- coding: utf-8 -*-
 import os
-import numpy as np
-import cv2
 from tqdm import tqdm
 from utils import imageutils
 
-
-""" auxilary functions """
 # unwarp corodinates
 def warpCoord(Minv, pt):
     out = np.matmul(Minv, (pt[0], pt[1], 1))
     return np.array([out[0]/out[2], out[1]/out[2]])
-""" end of auxilary functions """
 
 
 def getDetBoxes_core(textmap, linkmap, text_threshold, link_threshold, low_text):
@@ -249,10 +237,6 @@ def adjustResultCoordinates(polys, ratio_w, ratio_h, ratio_net = 2):
                 polys[k] *= (ratio_w * ratio_net, ratio_h * ratio_net)
     return polys
 
-
-
-
-# borrowed from https://github.com/lengstrom/fast-style-transfer/blob/master/src/utils.py
 def get_files(img_dir):
     imgs, masks, xmls = list_files(img_dir)
     return imgs, masks, xmls
@@ -265,14 +249,7 @@ def list_files(in_path):
         for file in filenames:
             filename, ext = os.path.splitext(file)
             ext = str.lower(ext)
-            if ext == '.jpg' or ext == '.jpeg' or ext == '.gif' or ext == '.png' or ext == '.pgm':
-                img_files.append(os.path.join(dirpath, file))
-            elif ext == '.bmp':
-                mask_files.append(os.path.join(dirpath, file))
-            elif ext == '.xml' or ext == '.gt' or ext == '.txt':
-                gt_files.append(os.path.join(dirpath, file))
-            elif ext == '.zip':
-                continue
+            img_files.append(os.path.join(dirpath, file))
     return img_files, mask_files, gt_files
 
 def saveResult(img_file, img, boxes, dirname='./result/', verticals=None, texts=None):
