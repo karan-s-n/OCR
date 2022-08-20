e2b63a187c165ee1fea52250f3a8c9b846b07629
karan-s-n <111242026+karan-s-n@users.noreply.github.com>
2022-08-21 04:27:36 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,9 +1,1 @@
 
-if __name__ == '__main__':
-  conn,cur = connect('db/ocr_db')
-  dataframe = get_dataset("/content/drive/MyDrive/datasets/predict_images/raw_images/reports/dataset.csv",conn,"rawdata")
-  create_load(cur,conn,"rawdata")
-  fetch_data(cur,"rawdata")
-
-
-
