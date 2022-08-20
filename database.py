5bdd8f585d0564d68461349c3bdb2cfc77612fef
karan-s-n <111242026+karan-s-n@users.noreply.github.com>
2022-08-21 04:26:51 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,25 +1,3 @@
-import pandas as pd
-import sqlite3
-
-def connect(dbname):
-  conn = sqlite3.connect(dbname)
-  cur = conn.cursor()
-  return conn,cur
-
-def get_dataset(path,conn,tablename):
-  df = pd.read_csv(path)
-  df.to_sql(tablename, conn, if_exists='replace', index = False)
-  return df
-
-def create_load(cur,conn,tablename):
-  cur.execute(f'CREATE TABLE IF NOT EXISTS {tablename} (imagpath text, x number,y number,w number,h number , wordpath text , predicttext text)')
-  conn.commit()
-
-def fetch_data(cur,table):
-  cur.execute(f''' SELECT * FROM {table}''')
-  for row in cur.fetchall():
-      print (row)
-      break
 
 if __name__ == '__main__':
   conn,cur = connect('db/ocr_db')
